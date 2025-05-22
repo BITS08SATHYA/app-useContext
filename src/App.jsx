e83f5d394f7a78fa678fb691c02e0de404539250
Sathya <neelasathya06@gmail.com>
2025-05-22 20:30:38 +0530

    context scope inside and outside implemented

--- src/App.jsx
+++ src/App.jsx
@@ -14,19 +14,22 @@ function App() {
     }
 
   return (
-      <ThemeContext.Provider value={theme}>
-        <div style={{ border: '2px solid black' , padding: '20px'}}>
-            <h2>App (Parent)</h2>
-            <button onClick={toggleTheme}>Toggle Theme</button>
-        <ComponentA />
-        </div>
-      </ThemeContext.Provider>
+      <div>
+          <GlobalComponent />
+          <ThemeContext.Provider value={theme}>
+            <div style={{ border: '2px solid black' , padding: '20px'}}>
+                <h2>App (Parent)</h2>
+                <button onClick={toggleTheme}>Toggle Theme</button>
+            <ComponentA />
+            </div>
+          </ThemeContext.Provider>
+      </div>
   );
 }
 
 function ComponentA(){
     return (
-        <div style={{ border: '2px solid black' , padding: '20px'}}>
+        <div style={{ border: '2px solid green' , padding: '20px'}}>
             <h2>ComponentA (Child)</h2>
             <ComponentB />
         </div>
@@ -35,7 +38,7 @@ function ComponentA(){
 
 function ComponentB(){
     return (
-        <div style={{ border: '2px solid black' , padding: '20px'}}>
+        <div style={{ border: '2px solid blue' , padding: '20px'}}>
             <h2>ComponentB (Grand Child)</h2>
             <ThemedComponent />
         </div>
@@ -45,8 +48,18 @@ function ComponentB(){
 function ThemedComponent(){
     const theme = useContext(ThemeContext);
     return (
-        <div style={{ border: '2px solid black' , padding: '20px'}}>
-            <h2>ThemedComponent (Great Grand Child)</h2>
+        <div style={{ border: '2px solid purple' , padding: '20px'}}>
+            <h2>GlobalComponent (Great Grand Child)</h2>
+            <div>The current theme is: {theme} </div>
+        </div>
+    );
+}
+
+function GlobalComponent(){
+    const theme = useContext(ThemeContext);
+    return (
+        <div style={{ border: '2px solid purple' , padding: '20px'}}>
+            <h2>GlobalComponent (Outside Provider)</h2>
             <div>The current theme is: {theme} </div>
         </div>
     );
